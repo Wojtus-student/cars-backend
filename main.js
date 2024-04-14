09e0d075f6531c39abe100bef65a863cbe92d409
Kavka <152932575+JaKafka@users.noreply.github.com>
2024-04-14 21:29:05 +0200

    Initial Commit

---
+++ main.js
@@ -0,0 +1,29 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+//TODO - move to configuration
+const PORT = 9000;
+
+app.listen(PORT, () => {
+    console.log("Cars listening on port: ", PORT);
+});
+
+
+// TODO - integrate with Mongodb
+app.post('/user', (req, res) => {
+    res.status(200).send('OK - Tworzenie usera');
+});
+
+app.get('/user/:userId', (req, res) => {
+    res.status(200).send({login: 'JanKowal', mail:'jakowal@example.com'});
+});
+
+app.put('/user/:userId', (req, res) => {
+    res.status(200).send(`OK - Modyfikacja usera ${req.params.userId}`)
+});
+
+app.delete('/user/:userId', (req, res) => {
+    res.status(200).send(`OK - Usuwanie usera ${req.params.userId}`)
+});
