175b33ab97978fff379ddb1138416a1e84a9a914
Jakub Mikusek <j.mikusek01@gmail.com>
2024-04-23 20:35:16 +0200

    Added CRUD operations to modify trips

--- main.js
+++ main.js
@@ -1,17 +1,11 @@
 const express = require('express');
 const TripDB = require("./module/trip_db.js").TripDB;
 
-const tripDB = new TripDB();
 const app = express();
 app.use(express.json());
 
 //TODO - move to configuration
 const PORT = process.env.PORT || 9000;
-
-async function test(){
-	console.log(await tripDB.fetchTrips());
-}
-test();
 
 app.listen(PORT, () => {
     console.log("Cars listening on port: ", PORT);
@@ -32,4 +26,51 @@ app.put('/user/:userId', (req, res) => {
 
 app.delete('/user/:userId', (req, res) => {
     res.status(200).send(`OK - Usuwanie usera ${req.params.userId}`)
+});
+
+//------------------------Endpoints for CROP operations on trips------------------------//
+
+const tripDB = new TripDB();
+//Add trip
+app.post('/trip', async (req, res) => {
+	if(req.body == null){
+		res.status(400).send(JSON.stringify({message : "provided record is empty"}));
+	}
+	try{
+		let instertedID = await tripDB.addTrip(req.body);
+		res.status(200).send(JSON.stringify({_id : instertedID}));
+	}catch(e){
+		res.status(500).send();
+	}
+});
+//Find trip
+app.get('/trip/:tripID', async(req, res) => {
+	let foundRecord = tripDB.findTrip(req.params.tripID, res.body.user.role);
+	if(foundRecord == null){
+		res.status(404).send();
+	}
+	res.status(200).send(JSON.stringify({body : foundRecord}));
+});
+//Update trip
+app.put('/trip/:tripID', async (req, res) => {
+	if(req.body==null){
+		res.status(400).send(JSON.stringify({message : "provided update doesn't change anything"}));
+	}
+	try{
+		if(await tripDB.updateTrip(res.params.tripID, req.body)){
+			res.status(200).send();
+		}
+		res.status(404).send();
+	}catch(e){
+		res.status(500).send();
+	}
+});
+//Delete trip
+app.delete('/trip/:tripID', async(req, res) => {
+	try{
+		tripDB.deleteTrip(res.params.tripID, res.body.user.role);
+		res.status(200).send();
+	}catch(e){
+		res.send(500).send();
+	}
 });
--- module/trip_db.js
+++ module/trip_db.js
@@ -1,3 +1,5 @@
+const { WriteError } = require("mongodb");
+
 const MongoClient = require("mongodb").MongoClient;
 
 module.exports = {
@@ -9,23 +11,50 @@ module.exports = {
 				this.carsCollection = this.client.db().collection("cars");
 				this.tripCollection = this.client.db().collection("trip");
 			}catch(e){
-				throw Error("unable to connect to database " + e);
+				console.log("unable to connect to database " + e);
 			}
 		}
-		async fetchCars(){
-			return await this.carsCollection.find().toArray();
+		async fetchCars(username = null){
+			if(username == null){
+				return await this.carsCollection.find().toArray();
+			}
+			return await this.carsCollection.find({username : username}).toArray();
 		}
-		async fetchTrips(){
-			return await this.tripCollection.find().toArray();
+		async fetchTrips(username = null){
+			if(username == null){
+				return await this.tripCollection.find().toArray();
+			}
+			return await this.tripCollection.find({username : username}).toArray();
 		}
-		async addTrip(record){
-			try{
-				let res = await this.tripCollection.insertOne(record);
-				if(res.acknowledged == false){
-					throw WriteError("unable to add trip");
+		async addTrip(record, role){
+			if(role != 'admin' || 'driver')
+				throw Error("Unauthorized user");
+			let res = await this.tripCollection.insertOne(record);
+			if(res.insertedId == 0){
+				throw WriteError("unable to add trip");
+			}
+			return res.insertedId;
+		}
+		async findTrip(id){
+			return await this.tripCollection.findOne({_id : id});
+		}
+		async updateTrip(id, recordNew){
+			let found = await this.findTrip(id);
+			if(found){
+				let res = await this.tripCollection.replaceOne(found, recordNew);
+				if(res.modifiedCount != 1){
+					throw WriteError("unable to update trip");
 				}
-			}catch(e){
-				print(e);
+				return true;
+			}
+			return false;
+		}
+		async deleteTrip(id, role){
+			if(role != 'admin' || 'driver')
+				throw Error("Unauthorized user");
+			let res = await this.tripCollection.deleteOne({_id : id});
+			if(res.deletedCount == 0){
+				throw WriteError("unable to delete record with id :" + id);
 			}
 		}
 	}
