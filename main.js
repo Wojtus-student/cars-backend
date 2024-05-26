c23d61e4d3e9e0bb39f6f84817535f93651976f1
Jakub Mikusek <j.mikusek01@gmail.com>
2024-05-26 23:44:10 +0200

    created api for scrapping vehicle data from CEPIK and reapired trip tests

--- main.js
+++ main.js
@@ -10,14 +10,13 @@ app.use(express.json());
 //TODO - move to configuration
 const PORT = process.env.PORT || 9000;
 
-app.listen(PORT, () => {
-    console.log("Cars listening on port: ", PORT);
-});
+// app.listen(PORT, () => {
+//     console.log("Cars listening on port: ", PORT);
+// });
 
 udb = new UserDB();
 
-app.post('/auth')
-{
+app.post('/auth', async (res, req) => {
     try {
         let foundUser = await udb.authUser(req.body);
         res.status(200).send(foundUser);
@@ -30,7 +29,8 @@ app.post('/auth')
             res.status(500).send('Internal server error');
         }
     }
-}
+
+});
 
 app.get('/user', async (req, res) => {
     try {
@@ -105,34 +105,33 @@ app.delete('/user/:userId', (req, res) => {
     res.status(200).send(`OK - Usuwanie usera ${req.params.userId}`)
 });
 
-//------------------------Endpoints for CROP operations on trips------------------------//
+//------------------------Endpoints for CRUD operations on trips------------------------//
 
 //TODO: change properties after getting template of body requests.
 //TODO: add more sophisticated ways of searching for trip
-//TODO: add endpoints for searching for cars
 const tripDB = new TripDB();
 //Add trip
 app.post('/trip', async (req, res) => {
     if (req.body == {}) {
-        res.status(400).send(JSON.stringify({ message: "provided record is empty" }));
+        res.status(400).send({ message: "provided record is empty" });
     }
     else {
         try {
-            let instertedID = await tripDB.addTrip(req.body, res.body.user.role);
-            res.status(200).send(JSON.stringify({ _id: instertedID }));
+            let instertedID = await tripDB.addTrip(req.body, req.body.user.role);
+            res.status(200).send({ _id: instertedID });
         } catch (e) {
-            res.status(500).send(JSON.stringify({ message: e.message }));
+            res.status(500).send({ message: e.message });
         }
     }
 });
 //Find trip
-app.get('/trip/:tripID', async (req, res) => {
-    let foundRecord = await tripDB.findTrip(req.params.tripID);
+app.get('/trip/:tripFilter', async (req, res) => {
+    let foundRecord = await tripDB.findTrip(req.params.tripFilter);
     if (foundRecord == null) {
         res.status(404).send();
     }
     else {
-        res.status(200).send(JSON.stringify({ body: foundRecord }));
+        res.status(200).send({ body: foundRecord });
     }
 });
 //Find all trips
@@ -142,35 +141,92 @@ app.get('/trip', async (req, res) => {
         res.status(404).send();
     }
     else {
-        res.status(200).send(JSON.stringify({ body: foundRecords }));
+        res.status(200).send({ body: foundRecords });
     }
 });
 //Update trip
 app.put('/trip/:tripID', async (req, res) => {
     if (req.body == null) {
-        res.status(400).send(JSON.stringify({ message: "provided update doesn't change anything" }));
+        res.status(400).send({ message: "provided update doesn't change anything" });
     }
     else {
         try {
-            if (await tripDB.updateTrip(res.params.tripID, req.body)) {
+            if (await tripDB.updateTrip(req.params.tripID, req.body)) {
                 res.status(200).send();
             }
             res.status(404).send();
         } catch (e) {
-            res.status(500).send(JSON.stringify({ message: e.message }));
+            res.status(500).send({ message: e.message });
         }
     }
 });
 //Delete trip
 app.delete('/trip/:tripID', async (req, res) => {
     try {
-        await tripDB.deleteTrip(res.params.tripID, res.body.user.role);
+        await tripDB.deleteTrip(req.params.tripID, req.body.user.role);
         res.status(200).send();
     } catch (e) {
-        res.status(500).send(JSON.stringify({ message: e.message }));
+        res.status(500).send({ message: e.message });
     }
 });
-
+//Add car
+app.get('/car', async (req, res) => {
+	req.body = {
+		registration : "szy44416",
+		vin : "knaba24429t732164",
+		registration_date : "16.04.2009",
+		user:{
+			role: "driver",
+		}
+	};
+    if (req.body == {}) {
+        res.status(400).send({ message: "provided record is empty" });
+    }
+    else {
+        try {
+            let instertedID = await tripDB.addCar(req.body, req.body.user.role);
+            // res.status(200).send(JSON.stringify({ _id: instertedID }));
+            res.status(200).send(instertedID);
+        } catch (e) {
+            res.status(500).send({ message: e.message });
+        }
+    }
+});
+//Find car
+app.get('/car/:carFilter', async (req, res) => {
+    let foundRecord = await tripDB.findCar(req.params.carFilter);
+    if (foundRecord == null) {
+        res.status(404).send();
+    }
+    else {
+        res.status(200).send({ body: foundRecord });
+    }
+});
+//Update car
+app.put('/car/:carID', async (req, res) => {
+    if (req.body == null) {
+        res.status(400).send({ message: "provided update doesn't change anything" });
+    }
+    else {
+        try {
+            if (await tripDB.updateCar(req.params.carID, req.body)) {
+                res.status(200).send();
+            }
+            res.status(404).send();
+        } catch (e) {
+            res.status(500).send({ message: e.message });
+        }
+    }
+});
+//Delete trip
+app.delete('/car/:carID', async (req, res) => {
+    try {
+        await tripDB.deleteCar(req.params.carID, req.body.user.role);
+        res.status(200).send();
+    } catch (e) {
+        res.status(500).send({ message: e.message });
+    }
+});
 module.exports = app;
 
 if (require.main === module) {
--- module/trip_db.js
+++ module/trip_db.js
@@ -1,6 +1,8 @@
 const { WriteError } = require("mongodb");
 
 const MongoClient = require("mongodb").MongoClient;
+const consumers = require("stream/consumers");
+const puppeteer = require("puppeteer");
 
 module.exports = {
 	TripDB : class{
@@ -10,53 +12,120 @@ module.exports = {
 				this.client.connect();
 				this.carsCollection = this.client.db().collection("cars");
 				this.tripCollection = this.client.db().collection("trip");
+				console.log("DEBUG: Connected to database");
 			}catch(e){
 				console.log("unable to connect to database " + e);
 			}
 		}
-		async fetchCars(username = null){
-			if(username == null){
-				return await this.carsCollection.find().toArray();
-			}
-			return await this.carsCollection.find({username : username}).toArray();
-		}
-		async fetchTrips(username = null){
-			if(username == null){
-				return await this.tripCollection.find().toArray();
-			}
-			return await this.tripCollection.find({username : username}).toArray();
-		}
-		async addTrip(record, role){
+//--------------------------------------CRUD ABSTRACT--------------------------------------//
+//--------------------------------------CRUD ABSTRACT--------------------------------------//
+//--------------------------------------CRUD ABSTRACT--------------------------------------//
+		async Create(collection, record, role){
 			if(role != 'admin' || 'driver')
 				throw Error("Unauthorized user");
-			let res = await this.tripCollection.insertOne(record);
-			if(res.insertedId == 0){
-				throw WriteError("unable to add trip");
+			if(await collection.findOne(record) == null){
+				let res = await collection.insertOne(record);
+				if(res.insertedId == 0){
+					throw WriteError("Unable to add " + JSON.stringify(record));
+				}
+			}
+			else{
+				throw WriteError("Record already exists in a database");
 			}
 			return res.insertedId;
 		}
-		async findTrip(id){
-			return await this.tripCollection.findOne({_id : id});
+		async Read(collection, username){
+			if(username == null){
+				return await collection.find().toArray();
+			}
+			return await collection.find({username : username}).toArray();
 		}
-		async updateTrip(id, recordNew){
-			let found = await this.findTrip(id);
+		async Update(collection, filter, recordNew, role){
+			if(role != 'admin' || 'driver')
+				throw Error("Unauthorized user");
+			let found = await collection.find(filter).toArray();
 			if(found){
-				let res = await this.tripCollection.replaceOne(found, recordNew);
+				let res = await collection.replaceOne(found, recordNew);
 				if(res.modifiedCount != 1){
-					throw WriteError("unable to update trip");
+					throw WriteError("Unable to update " + JSON.stringify(found));
 				}
 				return true;
 			}
 			return false;
 		}
-		async deleteTrip(id, role){
+		async Delete(collection, id, role){
 			if(role != 'admin' || 'driver')
 				throw Error("Unauthorized user");
-			let res = await this.tripCollection.deleteOne({_id : id});
+			let res = await collection.deleteOne({_id : id});
 			if(res.deletedCount == 0){
-				throw WriteError("unable to delete record with id :" + id);
+				throw WriteError("Unable to delete record with id :" + id);
 			}
 		}
+//----------------------------------------CARS---------------------------------------//
+//----------------------------------------CARS---------------------------------------//
+//----------------------------------------CARS---------------------------------------//
+		async fetchCars(username = null){
+			return this.Read(this.carsCollection, username);
+		}
+		async addCar(record, role){
+			if(role != "driver"){
+				throw WriteError("User not authorized to add cars");
+			}
+			const browser = await puppeteer.launch();
+			const page = await browser.newPage();
+			await page.goto("https://historiapojazdu.gov.pl");
+			await page.evaluate((record) =>{
+				document.getElementById("_historiapojazduportlet_WAR_historiapojazduportlet_:rej").value = record.registration;
+				document.getElementById("_historiapojazduportlet_WAR_historiapojazduportlet_:vin").value = record.vin;
+				document.getElementById("_historiapojazduportlet_WAR_historiapojazduportlet_:data").value = record.registration_date;
+			}, record);
+			try{
+				await page.evaluate(()=>{
+					document.getElementById("_historiapojazduportlet_WAR_historiapojazduportlet_:btnSprawdz").click();
+				});
+			}catch(e){}
+			await page.waitForNavigation({waitUntil: 'load'});
+			let vehicleData = await page.evaluate((record)=>{
+				let data = {data_rejestracji : record.registration_date, numer_rejestracyjny : record.registration};
+				let defaultKeys = ["marka","model","rodzaj","podrodzaj","pojemnosc","paliwo"];
+				for(let i = 0; i < defaultKeys.length; i++){
+					data[defaultKeys[i]] = document.getElementById("_historiapojazduportlet_WAR_historiapojazduportlet_:j_idt10:" + defaultKeys[i]).innerHTML;
+				}
+				let details = document.getElementsByClassName("group-text data")[0].querySelectorAll("p");
+				for(let i = 0; i < details.length; i++){
+					data[details[i].className] = details[i].childNodes[1].innerText;
+				}
+				return data;
+			}, record);
+			await browser.close();
+			return vehicleData;
+		}
+		async findCar(record){
+			return await this.carsCollection.findOne(record);
+		}
+		async updateCar(id, recordNew, role){
+			return this.Update(this.carsCollection,{_id : id}, recordNew, role);
+		}
+		async deleteCar(id, role){
+			return this.Delete(this.carsCollection,	id, role);
+		}
+//----------------------------------------TRIPS---------------------------------------//
+//----------------------------------------TRIPS---------------------------------------//
+//----------------------------------------TRIPS---------------------------------------//
+		async fetchTrips(username = null){
+			return this.Read(this.tripCollection, username);
+		}
+		async addTrip(record, role){
+			return this.Create(this.tripCollection,record,role);
+		}
+		async findTrip(record){
+			return await this.tripCollection.findOne(record);
+		}
+		async updateTrip(id, recordNew, role){
+			return this.Update(this.tripCollection,{_id : id}, recordNew, role);
+		}
+		async deleteTrip(id, role){
+			return this.Delete(this.tripCollection,	id, role);
+		}
 	}
-
 }
